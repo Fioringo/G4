c1503be11cd7ed307d84a5c32135015328ca4aaa
Matt <matthew.wong@outlook.com>
2019-04-27 17:21:34 -0400

    Finished file upload code

--- backend/upload.py
+++ backend/upload.py
@@ -4,6 +4,11 @@ from werkzeug.utils import secure_filename
 
 upload = Blueprint(__name__, "upload")
 
+ALLOWED_EXTENSIONS = set(['txt', 'pdf', 'png', 'jpg', 'jpeg', 'gif'])
+
+def allowed_file(filename):
+    return '.' in filename and filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
+
 @upload.route("/api/upload", methods=['GET', 'POST'])
 def upload_():
 
@@ -12,7 +17,7 @@ def upload_():
         # check if the post request has the file part
         if 'file' not in request.files:
             message = 'No file part'
-            file = request.files['file']
+        file = request.files['file']
         if file.filename == '':
             message = 'No file selected for uploading'
         if file and allowed_file(file.filename):
